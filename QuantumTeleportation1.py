e78530db11519a35cb699f4e2f6e605447493fb8
Mayukh Chatterjee <54723572+mayukh00@users.noreply.github.com>
2022-01-18 17:44:40 +0530

    Quantum Teleportation using Qiskit.
    Quantum Teleportation of a single quantum bit over a full-entangled channel.

---
+++ QuantumTeleportation1.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+# Do the necessary imports
+import numpy as np
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit import IBMQ, Aer, transpile, assemble
+from qiskit.visualization import plot_histogram, plot_bloch_multivector, array_to_latex
+from qiskit.extensions import Initialize
+from qiskit.ignis.verification import marginal_counts
+from qiskit.quantum_info import random_statevector
+
+
+# In[11]:
+
+
+def create_bell_pair(qc, a, b):
+    """Creates a bell pair in qc using qubits a & b"""
+    qc.h(a) # Put qubit a into state |+>
+    qc.cx(a,b) # CNOT with a as control and b as target
+    
+
+
+# In[12]:
+
+
+def alice_gates(qc, psi, a):
+    qc.cx(psi, a)
+    qc.h(psi)
+
+
+# In[13]:
+
+
+def measure_and_send(qc, a, b):
+    """Measures qubits a & b and 'sends' the results to Bob"""
+    qc.barrier()
+    qc.measure(a,0)
+    qc.measure(b,1)
+
+
+# In[14]:
+
+
+# This function takes a QuantumCircuit (qc), integer (qubit)
+# and ClassicalRegisters (crz & crx) to decide which gates to apply
+def bob_gates(qc, qubit, crz, crx):
+    # Here we use c_if to control our gates with a classical
+    # bit instead of a qubit
+    qc.x(qubit).c_if(crx, 1) # Apply gates if the registers 
+    qc.z(qubit).c_if(crz, 1) # are in the state '1'
+
+
+# In[15]:
+
+
+# Create random 1-qubit state
+psi = random_statevector(2)
+
+# Display it nicely
+display(array_to_latex(psi, prefix="|\\psi\\rangle ="))
+# Show it on a Bloch sphere
+plot_bloch_multivector(psi)
+
+
+# In[16]:
+
+
+init_gate = Initialize(psi)
+init_gate.label = "init"
+
+
+# In[17]:
+
+
+## SETUP
+qr = QuantumRegister(3, name="q")   # Protocol uses 3 qubits
+crz = ClassicalRegister(1, name="crz") # and 2 classical registers
+crx = ClassicalRegister(1, name="crx")
+qc = QuantumCircuit(qr, crz, crx)
+
+## STEP 0
+# First, let's initialize Alice's q0
+qc.append(init_gate, [0])
+qc.barrier()
+
+## STEP 1
+# Now begins the teleportation protocol
+create_bell_pair(qc, 1, 2)
+qc.barrier()
+
+## STEP 2
+# Send q1 to Alice and q2 to Bob
+alice_gates(qc, 0, 1)
+
+## STEP 3
+# Alice then sends her classical bits to Bob
+measure_and_send(qc, 0, 1)
+
+## STEP 4
+# Bob decodes qubits
+bob_gates(qc, 2, crz, crx)
+
+# Display the circuit
+qc.draw()
+
+
+# In[18]:
+
+
+sim = Aer.get_backend('aer_simulator')
+qc.save_statevector()
+out_vector = sim.run(qc).result().get_statevector()
+plot_bloch_multivector(out_vector)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
